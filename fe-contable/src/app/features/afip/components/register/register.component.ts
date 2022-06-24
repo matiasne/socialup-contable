72078a5e0ae32f02d967b4c124cdde52a437b308
Matias <matiasnegri85@gmail.com>
2022-06-23 22:23:48 -0300

    work in progress

--- fe-contable/src/app/features/afip/components/register/register.component.ts
+++ fe-contable/src/app/features/afip/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { SelectedService } from 'src/app/services/global/selected.service';
+import { AfipService } from '../../services/afip.service';
 
 @Component({
   selector: 'app-register',
@@ -7,8 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public formData:FormGroup;
+  public isSubmited=false;
+  private obsBusiness:any;
+  
+  constructor(
+    private afipService: AfipService,
+    private selectedService: SelectedService,
+  ) { 
+    this.formData = new FormGroup({
+      businessId: new FormControl('',Validators.required),
+      email: new FormControl('',Validators.compose([Validators.required,Validators.email])),
+      password: new FormControl('',([Validators.required])),
+      puntoVenta: new FormControl('',([Validators.required])),
+      fileKey: new FormControl('',([Validators.required])),
+      filePem: new FormControl('',([Validators.required])),
+    });
+  }
 
   ngOnInit() {}
+
+  ionViewWillEnter(){
+    this.obsBusiness = this.selectedService.obsSelectedBusiness().subscribe({
+      next: (data:any)=>{
+        this.formData.patchValue({
+          businessId: data.id,
+        })
+      }
+    })
+  }
+
+  ionViewWillLeave(){
+    this.obsBusiness.unsubscribe();
+  }
+
+  onSubmit(){
+    this.isSubmited=true;  
+    if(this.formData.valid){
+      this.afipService.register(this.formData.value).subscribe({
+        next: (data:any)=>{
+          console.log(data)
+        }
+      })
+    }
+  }
 
 }
--- fe-contable/src/app/features/afip/services/afip.service.ts
+++ fe-contable/src/app/features/afip/services/afip.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BaseCRUDService } from 'src/app/services/base-crud.service';
 import { GLOBAL } from 'src/app/services/global';
+import { HelperService } from 'src/app/services/helpers.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,7 @@ export class AfipService extends BaseCRUDService{
 
   constructor(
     public _http: HttpClient,
+    private helperService: HelperService,
   ) {
     super(_http)
     this.url = GLOBAL.url+'/afip';
@@ -23,5 +25,10 @@ export class AfipService extends BaseCRUDService{
       password:password
     }
     return this.post(this.url+'/connect',params)
+  }
+  
+  register(data:any){
+    let formData = this.helperService.toFormData(data)
+    return this.post(this.url+'/register',formData)
   }
 }
