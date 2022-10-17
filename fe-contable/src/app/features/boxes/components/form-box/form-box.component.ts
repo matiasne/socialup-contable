23f06e273192362db96cacf56e677b65158f019a
davidjrosso <david.rosso@gmail.com>
2022-10-17 16:49:50 -0300

    created components

---
+++ fe-contable/src/app/features/boxes/components/form-box/form-box.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-box',
+  templateUrl: './form-box.component.html',
+  styleUrls: ['./form-box.component.scss'],
+})
+export class FormBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ fe-contable/src/app/features/boxes/components/list-box/list-box.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-box',
+  templateUrl: './list-box.component.html',
+  styleUrls: ['./list-box.component.scss'],
+})
+export class ListBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
