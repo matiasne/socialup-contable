7b7b18a096c4a9c6c083cf8c93e162bcb576fd88
Nahuel Rosso <nahuelgrosso25@gmail.com>
2022-10-25 16:51:29 -0300

    Resuelto, Dodal Detail

--- fe-contable/src/app/features/sales/components/modal-detail/modal-detail.component.ts
+++ fe-contable/src/app/features/sales/components/modal-detail/modal-detail.component.ts
@@ -24,28 +24,25 @@ export class ModalDetailComponent implements OnInit {
   }
   renameSale() {
     this.sale.payments.forEach((element) => {
-      if (element.type == paymentTypes.cash.name) {
+      if (element.type == paymentTypes.cash.value) {
         element['tipo'] = 'Efectivo';
       }
 
-      if (element.type == paymentTypes.creditCard.name) {
+      if (element.type == paymentTypes.creditCard.value) {
         element['tipo'] = 'Tarjeta Crédito';
       }
-      if (element.type == paymentTypes.debitCard.name) {
+      if (element.type == paymentTypes.debitCard.value) {
         element['tipo'] = 'Tarjeta Debito ';
       }
 
-      if (element.type == paymentTypes.personalAccount.name) {
+      if (element.type == paymentTypes.personalAccount.value) {
         element['tipo'] = 'Cuenta Corriente';
       }
-      if (element.type == paymentTypes.transfer.name) {
+      if (element.type == paymentTypes.transfer.value) {
         element['tipo'] = 'Transeferencia';
       }
-      if (element.type == paymentTypes.check.name) {
+      if (element.type == paymentTypes.check.value) {
         element['tipo'] = 'Cheque';
-      }
-      if (element.type == paymentTypes.empty.name) {
-        element['tipo'] = 'empty';
       }
     });
   }
