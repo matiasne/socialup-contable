5439a980426287ed895ecafa529f2f7d0d1ec8d1
Nahuel Rosso <nahuelgrosso25@gmail.com>
2023-02-03 15:55:15 -0300

    Enrutado

--- fe-contable-react/src/Routes.tsx
+++ fe-contable-react/src/Routes.tsx
@@ -2,14 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import { FormForgotPassword } from "./pages/forgot-password/form-forgotpassword";
 import { FormLogin } from "./pages/formLogin/formLogin";
 import { FormRegister } from "./pages/formRegister/formRegister";
+import { RouterLayout } from "./shared/NavBar/RouterLayout";
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<div>Ingrese la url de la pagina</div>} />
+      <Route path="/" element={<RouterLayout />}>
+        <Route path="/nose" element={<div>Ingrese la url de la pagina</div>} />
+        <Route path="/forgot-password" element={<FormForgotPassword />} />
+      </Route>
+
       <Route path="/login" element={<FormLogin />} />
       <Route path="/register" element={<FormRegister />} />
-      <Route path="/forgot-password" element={<FormForgotPassword />} />
     </Routes>
   );
 };
