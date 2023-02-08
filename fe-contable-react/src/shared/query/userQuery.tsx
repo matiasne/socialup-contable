2d91ef7f2b90520c64d0ed044629b16a849e096d
CanarioTOMAS <tomimartine.34@gmail.co>
2023-02-08 15:42:21 -0300

    install

---
+++ fe-contable-react/src/shared/query/userQuery.tsx
@@ -0,0 +1,14 @@
+import { gql } from "@apollo/client";
+
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
+
+export { GET_LOCATIONS };
