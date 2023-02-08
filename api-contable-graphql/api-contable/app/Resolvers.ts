2a2cfbbe4c5f24cbb4fcedd53adfedc7daa8d754
CanarioTOMAS <tomimartine.34@gmail.co>
2023-02-08 15:48:01 -0300

    work

--- api-contable-graphql/api-contable/app/Resolvers.ts
+++ api-contable-graphql/api-contable/app/Resolvers.ts
@@ -1,9 +1,7 @@
 import Person from "./schema/person";
 import User from "./schema/user";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 import { UserInputError } from "apollo-server-core";
-
-
 
 const Resolvers = {
   Query: {
@@ -11,47 +9,50 @@ const Resolvers = {
     allPersons: async (root: any, args: any) => {
       return Person.collection.find({});
     },
-    findPerson: (root: any, args: any) =>{
+    findPerson: (root: any, args: any) => {
       const { name } = args;
       return Person.collection.findOne({ name });
-    }
+    },
   },
   Mutation: {
     //create our mutation:
     addPerson: (root: any, args: any) => {
       const newPerson = new Person({ ...args });
-      
+
       return newPerson.save(); //return the new object's result
     },
     createUser: (root: any, args: any) => {
-      const user = new User({ username: args.username, password: args.password})
+      const user = new User({
+        username: args.username,
+        password: args.password,
+      });
 
-      return user.save().catch(error => {
+      return user.save().catch((error) => {
         throw new UserInputError(error.message, {
-          invalidArgs: args
-        })
-      })
+          invalidArgs: args,
+        });
+      });
     },
-    login: async (root:any, args:any) => {
-      const user = await User.findOne({ username: args.username, password: args.password})
+    login: async (root: any, args: any) => {
+      const user = await User.findOne({
+        username: args.username,
+        password: args.password,
+      });
 
-      console.log(user)
-
-      if (!user){
-        throw new UserInputError('wrong credentials')
+      if (!user) {
+        throw new UserInputError("wrong credentials");
       }
 
       const userForToken = {
         username: user.username,
         password: user.password,
-        id: user._id
-      }
+        id: user._id,
+      };
 
       return {
-        value: jwt.sign(userForToken, "SOCIALUP")
-      }
-    }
+        value: jwt.sign(userForToken, "SOCIALUP"),
+      };
+    },
   },
-    
 };
 export default Resolvers;
