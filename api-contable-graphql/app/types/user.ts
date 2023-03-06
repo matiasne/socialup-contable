9b330fe8bc3a96da75967c254cda7213100998bf
Diego Nivoli <dnivoli@gmail.com>
2023-03-06 15:43:44 -0300

    Work in Progres

---
+++ api-contable-graphql/app/types/user.ts
@@ -0,0 +1,39 @@
+import { gql } from "apollo-server-express";
+
+module.exports = gql`
+  type User {
+    name: String!
+    surname: String!
+    email: String!
+    password: String!
+    role: String
+    image: String
+    address: String
+    gender: String
+    phone: String
+    id: ID!
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Token {
+    value: String!
+  }
+
+  type Mutation {
+    createUser(
+      name: String!
+      surname: String!
+      email: String!
+      password: String!
+      role: String
+      image: String
+      address: String
+      gender: String
+      phone: String
+    ): User
+    login(email: String!, password: String!): Token
+  }
+`;
