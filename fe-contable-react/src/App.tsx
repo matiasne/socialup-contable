f13e417573635456a55552bd0f67946700944152
Nicolas Falco <111098148+NicolasFalco@users.noreply.github.com>
2023-03-22 16:19:57 -0300

    avatar-terminado

--- fe-contable-react/src/App.tsx
+++ fe-contable-react/src/App.tsx
@@ -2,6 +2,8 @@ import "./App.css";
 import "./pages/formRegister/formRegister.tsx";
 import { AppRouter } from "./Routes";
 import { BrowserRouter } from "react-router-dom";
+import { Avatar } from "@mui/material";
+import AvatarWithInput from "./shared/Components/avatar";
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <header className="App-header">
         <BrowserRouter>
           <AppRouter />
+          <AvatarWithInput />
         </BrowserRouter>
       </header>
     </div>
