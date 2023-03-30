f4b2a45291920db1aea06d2053afc10c289ef534
CanarioTOMAS <tomimartine.34@gmail.co>
2023-03-30 16:16:59 -0300

    list-styled-finish

--- fe-contable-react/src/shared/components/list-item/list-item.tsx
+++ fe-contable-react/src/shared/components/list-item/list-item.tsx
@@ -1,4 +1,4 @@
-import { List, ListItem } from "@mui/material";
+import { Divider, List, ListItem } from "@mui/material";
 import React from "react";
 
 interface IProps {
@@ -10,9 +10,21 @@ export const ListItems = ({ items, renderItem }: IProps) => {
   console.log(items);
   return (
     <div>
-      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+      <List
+        sx={{
+          width: "100%",
+          bgcolor: "background.paper",
+          textAlign: "center",
+          margin: "auto",
+        }}
+      >
         {items.map((item: any, i) => {
-          return <ListItem key={i}>{renderItem(item)}</ListItem>;
+          return (
+            <>
+              <ListItem key={i}>{renderItem(item)}</ListItem>
+              <Divider />
+            </>
+          );
         })}
       </List>
     </div>
