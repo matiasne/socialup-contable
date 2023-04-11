3bb31de970bc67d279e92280362a1b1f62bfe630
Agustin Sala <agustinsala0@gmail.com>
2023-04-11 16:24:35 -0300

    item businesslisto

--- fe-contable-react/src/Routes.tsx
+++ fe-contable-react/src/Routes.tsx
@@ -4,16 +4,11 @@ import FormClient from "./features/Clients/components/FormClient/formClient";
 import { ListProduct } from "./features/products/components/list-product/list-product";
 import { BoxCreate } from "./pages/createBox/create-box.page";
 import { SaleCreate } from "./pages/createSale/create-sale.page";
-import { getSessionServices } from "./auth/services/session.service";
 import { FormProductComponent } from "./features/products/components/formProduct";
 import { FormForgotPassword } from "./pages/forgot-password/form-forgotpassword";
 import { FormLogin } from "./pages/formLogin/formLogin";
-import { FormProfile } from "./pages/formProfile/formProfile";
 import { FormRegister } from "./pages/formRegister/formRegister";
 import { Dialog } from "./shared/Components/modal/Dialogs";
-import ItemBox from "./features/Boxes/Components/item-box/item-box";
-import { ListClient } from "./features/Clients/components/ListClient/listClient";
-import { ListBox } from "./features/Boxes/Components/list-box/list-box";
 
 export const AppRouter = () => {
   return (
