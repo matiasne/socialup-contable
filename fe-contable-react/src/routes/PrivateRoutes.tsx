5cf0ae485755fcda30a14df14eda26b3d2e69f97
CanarioTOMAS <tomimartine.34@gmail.co>
2023-06-07 16:27:53 -0300

    Routes privates and publics

--- fe-contable-react/src/routes/PrivateRoutes.tsx
+++ fe-contable-react/src/routes/PrivateRoutes.tsx
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { getSessionServices } from "../auth/services/authService";
 
-export const ProtectedRoute = () => {
+export const PrivateRoute = () => {
   const session = getSessionServices();
   console.log(session);
   return session ? <Outlet /> : <Navigate to="/login" />;
--- fe-contable-react/src/routes/Routes.tsx
+++ fe-contable-react/src/routes/Routes.tsx
@@ -3,22 +3,23 @@ import FormClient from "../features/Clients/components/form-client/formClient";
 import { ListProduct } from "../features/products/components/list-product/list-product";
 import { BoxCreate } from "../pages/createBox/create-box.page";
 import { SaleCreate } from "../pages/createSale/create-sale.page";
-import { FormForgotPassword } from "../pages/forgot-password/form-forgotpassword";
 import { FormLogin } from "../pages/formLogin/formLogin";
 import { FormRegister } from "../pages/formRegister/formRegister";
 import { Dialog } from "../shared/Components/modal/Dialogs";
 import { ListBox } from "../features/Boxes/Components/list-box/list-box";
 import { BusinessCreate } from "../pages/createBusiness/create-business.page";
 import FormProductComponent from "../features/products/components/form-product/formProduct";
-import { ProtectedRoute } from "./PrivateRoutes";
-import { getSessionServices } from "../auth/services/authService";
+import { PrivateRoute } from "./PrivateRoutes";
 
 export const AppRouter = () => {
   return (
     <Routes>
+      {/*Rutas Publicas🔓   👇👇👇*/}
       <Route path="/login" element={<FormLogin />} />
       <Route path="/register" element={<FormRegister />} />
-      <Route path="/" element={<ProtectedRoute />}>
+
+      {/*Rutas privadas🔐   👇👇👇*/}
+      <Route path="/" element={<PrivateRoute />}>
         <Route path="/business" element={<BusinessCreate />} />
         <Route path="/sale" element={<SaleCreate />} />
         <Route
